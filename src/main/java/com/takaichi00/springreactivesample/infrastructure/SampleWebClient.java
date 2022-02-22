99fe3604fc19d2af5e4a5a867a4d69a739f5c3b4
TomoakiTakaichi <kiruhihhohu555@gmail.com>
2022-02-22 23:05:46 +0900

    chore: setup wiremock

---
+++ src/main/java/com/takaichi00/springreactivesample/infrastructure/SampleWebClient.java
@@ -0,0 +1,4 @@
+package com.takaichi00.springreactivesample.infrastructure;
+
+public class SampleWebClient {
+}
---
+++ src/test/java/com/takaichi00/springreactivesample/infrastructure/SampleWebClientTest.java
@@ -0,0 +1,37 @@
+package com.takaichi00.springreactivesample.infrastructure;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+class SampleWebClientTest {
+  protected final static WireMockServer wiremock = new WireMockServer(options().port(18080));
+
+  @BeforeAll
+  static void setupVariables() {
+    wiremock.start();
+    WireMock.configureFor(18080);
+  }
+
+  @BeforeEach
+  void setUp() {
+    wiremock.resetAll();
+  }
+
+  @AfterEach
+  void restartWireMockServer() {
+    if (!wiremock.isRunning()) {
+      wiremock.start();
+    }
+  }
+
+  @AfterAll
+  static void tearDown() {
+    wiremock.stop();
+  }
+}
